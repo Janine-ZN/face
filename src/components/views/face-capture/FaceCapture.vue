8389951e5ac56811b9f889a353292db613579733
Janine-ZN <Jainie_z@163.com>
2019-04-21 16:37:07 +0800

    对比信息

--- src/components/views/face-capture/FaceCapture.vue
+++ src/components/views/face-capture/FaceCapture.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row :gutter="20">
       <!-- 左侧 -->
-      <el-col :span="7">
+      <el-col :span="6">
         <!-- 左上 -->
         <el-row>
           <el-col>
@@ -57,12 +57,64 @@
           </el-row>
         </div>
       </el-col>
-      <el-col :span="17">
+      <el-col :span="18">
         <el-row>
           <el-col>
             <span class="title-font">对比信息</span>
           </el-col>
         </el-row>
+        <!-- 左下 -->
+        <div class="date bg-white">
+          <el-row :gutter="20">
+            <!-- 左侧 -->
+            <el-col :span="10">
+              <div class="img-group">
+                <div class="img-tip">相似度：0%</div>
+                <img src="@/assets/imgs/similar1.jpg" alt srcset>
+                <img src="@/assets/imgs/similar2.jpg" alt srcset>
+              </div>
+            </el-col>
+            <el-col :span="14">
+              <el-row>
+                <el-col :span="12">
+                  <el-row>
+                    <el-col>
+                      <ul class="ul-img-info">
+                        <li>
+                          <span class="img-info">姓名 :</span> 田思思
+                        </li>
+                        <li>
+                          <span class="img-info">性别 :</span> 女
+                        </li>
+                        <li>
+                          <span class="img-info">时间 :</span> 2017-09-05 14:03:25
+                        </li>
+                      </ul>
+                    </el-col>
+                  </el-row>
+                </el-col>
+                <el-col :span="12">
+                  <ul class="ul-img-info">
+                    <li>
+                      <span class="img-info">人脸库 :</span> 黑名单
+                    </li>
+                    <li>
+                      <span class="img-info">身份证 :</span> 342224199005261735
+                    </li>
+                    <li>
+                      <span class="img-info">阈值 :</span> 2017-09-05 14:03:25
+                    </li>
+                  </ul>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col>
+        <span class="title-font">人脸抓拍结果</span>
       </el-col>
     </el-row>
   </div>
@@ -77,31 +129,22 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.date {
-  margin-top: 10px;
-  border: 1px dashed $border_color;
-  min-height: 190px;
-}
-.bg-white {
-  background: $my_main_color;
-}
-.input-title {
-  position: relative;
-  left: 35px;
-  top: 10px;
-  font-size: 12px;
-  color: $font_secondary_color;
-  background: $my_main_color;
-  z-index: 999;
-}
-.btn-group {
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 20px;
+
+/* 该 ul 只用在当前页 */
+ul.ul-img-info {
+  padding: 0;
+  margin: 0.9375em 0;
+  list-style: none;
+  li {
+    padding: 0.9375em 0;
+  }
+  .img-info {
+    font-size: 0.875em;
+    color: $font_secondary_color;
+  }
 }
 </style>
 <style lang="scss">
-// @import '@common/style/mixin.scss';
 .date {
   .block {
     text-align: center !important;

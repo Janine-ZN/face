b40e88fa19a861c61c4b6b9cd98013130cd8d04f
Janine-ZN <Jainie_z@163.com>
2019-04-21 14:51:43 +0800

    自定义圆角按钮

--- src/components/views/face-capture/FaceCapture.vue
+++ src/components/views/face-capture/FaceCapture.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row :gutter="20">
       <!-- 左侧 -->
-      <el-col :span="6">
+      <el-col :span="7">
         <!-- 左上 -->
         <el-row>
           <el-col>
@@ -39,10 +39,30 @@
               </div>
             </el-col>
           </el-row>
+          <el-row>
+            <el-col>
+              <label>
+                <input type="radio" name="show">
+                <span>仅显示已识别结果</span>
+              </label>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col>
+              <div class="btn-group">
+                <button class="btn-primary">选好了，确认统计</button>
+                <button class="btn-dashed">重置</button>
+              </div>
+            </el-col>
+          </el-row>
         </div>
       </el-col>
-      <el-col :span="18">
-        <div class="grid-content bg-purple">55</div>
+      <el-col :span="17">
+        <el-row>
+          <el-col>
+            <span class="title-font">对比信息</span>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -74,19 +94,23 @@ export default {
   background: $my_main_color;
   z-index: 999;
 }
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+}
 </style>
 <style lang="scss">
+// @import '@common/style/mixin.scss';
 .date {
   .block {
     text-align: center !important;
   }
   .el-date-editor.el-input {
     width: 90% !important;
-    // margin: 15px auto !important;
   }
   .el-input__inner {
     height: 30px !important;
-    // background-color: transparent !important;
     border-radius: 25px !important;
     border: 1px dashed $border_color !important;
     padding-left: 118px !important;
@@ -95,7 +119,7 @@ export default {
     left: 90px;
   }
   .el-icon-time:before {
-    content: "\E608";//date
+    content: "\E608"; //date
     color: $main_color;
   }
   .el-input__icon {
